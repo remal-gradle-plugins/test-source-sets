7e2d70443d255d60584912883f9cdba4e38b4a21
Semyon Levin <levin.semen@gmail.com>
2021-04-30 14:18:26 +0300

    draft impl

---
+++ src/main/java/name/remal/gradleplugins/testsourcesets/v2/TestSourceSetContainer.java
@@ -0,0 +1,7 @@
+package name.remal.gradleplugins.testsourcesets.v2;
+
+import org.gradle.api.NamedDomainObjectContainer;
+import org.gradle.api.tasks.SourceSet;
+
+public interface TestSourceSetContainer extends NamedDomainObjectContainer<SourceSet> {
+}
