23664b546e950b24dc0549d111e8915f66a461f2
Semyon Levin <levin.semen@gmail.com>
2023-07-19 14:13:58 -0700

    [release-later] Gradle 8.3 support

--- src/functionalTest/java/name/remal/gradle_plugins/test_source_sets/TestSourceSetsPluginFunctionalTest.java
+++ src/functionalTest/java/name/remal/gradle_plugins/test_source_sets/TestSourceSetsPluginFunctionalTest.java
@@ -1,6 +1,5 @@
 package name.remal.gradle_plugins.test_source_sets;
 
-import static java.lang.String.join;
 import static name.remal.gradle_plugins.test_source_sets.TestSourceSetsPlugin.ALL_TESTS_TASK_NAME;
 import static name.remal.gradle_plugins.toolkit.StringUtils.escapeGroovy;
 import static name.remal.gradle_plugins.toolkit.testkit.GradleDependencyVersions.getCorrespondingKotlinVersion;
@@ -25,8 +24,7 @@ class TestSourceSetsPluginFunctionalTest {
             build.registerDefaultTask(ALL_TESTS_TASK_NAME);
 
             build.append("repositories { mavenCentral() }");
-            build.append(join(
-                "\n",
+            build.append(
                 "Dependency junitDependency = dependencies.create('junit:junit:4.13.2')",
                 "project.configurations.with { configurations ->",
                 "    testSourceSets.all { SourceSet sourceSet ->",
@@ -34,13 +32,67 @@ class TestSourceSetsPluginFunctionalTest {
                 "            .dependencies",
                 "            .add(junitDependency)",
                 "    }",
-                "}"
-            ));
+                "}",
+                "",
+                "tasks.withType(Test).configureEach {",
+                "    useJUnit()",
+                "    enableAssertions = true",
+                "    testLogging {",
+                "        showExceptions = true",
+                "        showCauses = true",
+                "        showStackTraces = true",
+                "        exceptionFormat = 'FULL'",
+                "        stackTraceFilters('GROOVY')",
+                "    }",
+                "}",
+                ""
+            );
+
+            build.append(
+                "file(\"src/test/java/pkg/JavaTest.java\").with {",
+                "    parentFile.mkdirs()",
+                "    write([",
+                "        'package pkg;',",
+                "        'import org.junit.Test;',",
+                "        'public class JavaTest {',",
+                "        '    @Test',",
+                "        '    public void test() {}',",
+                "        '}',",
+                "    ].join('\\n'), 'UTF-8')",
+                "}",
+                ""
+            );
+
+            build.append(
+                "file(\"src/additionalTest/java/pkg/JavaAdditionalTest.java\").with {",
+                "    parentFile.mkdirs()",
+                "    write([",
+                "        'package pkg;',",
+                "        'import org.junit.Test;',",
+                "        'public class JavaAdditionalTest {',",
+                "        '    @Test',",
+                "        '    public void test() {}',",
+                "        '}',",
+                "    ].join('\\n'), 'UTF-8')",
+                "}",
+                ""
+            );
         });
     }
 
     @Test
     void emptyBuildPerformsSuccessfully() {
+        project.getBuildFile().append(
+            "file(\"src/test/java\").deleteDir()",
+            "file(\"src/additionalTest/java\").deleteDir()",
+            ""
+        );
+
+        project.assertBuildSuccessfully();
+    }
+
+    @Test
+    void nonEmptyBuildPerformsSuccessfully() {
         project.assertBuildSuccessfully();
     }
 
@@ -64,12 +116,26 @@ class TestSourceSetsPluginFunctionalTest {
                 "dependencies { api 'org.jetbrains.kotlin:kotlin-stdlib:" + escapeGroovy(kotlinVersion) + "' }",
                 "file(\"src/main/kotlin/pkg/kotlinFile.kt\").with {",
                 "    parentFile.mkdirs()",
-                "    write('internal fun internalFunction(): Unit = TODO()', 'UTF-8')",
+                "    write([",
+                "        'package pkg',",
+                "        'internal fun internalFunction(): Unit {}',",
+                "    ].join('\\n'), 'UTF-8')",
                 "}",
-                "file(\"src/additionalTest/kotlin/pkg/kotlinFileTest.kt\").with {",
+                "",
+                "file(\"src/additionalTest/kotlin/pkg/KotlinAdditionalTest.kt\").with {",
                 "    parentFile.mkdirs()",
-                "    write('fun internalFunctionTest() = internalFunction()', 'UTF-8')",
-                "}"
+                "    write([",
+                "        'package pkg',",
+                "        'import org.junit.Test',",
+                "        'class KotlinAdditionalTest {',",
+                "        '    @Test',",
+                "        '    fun test() {',",
+                "        '        internalFunction()',",
+                "        '    }',",
+                "        '}',",
+                "    ].join('\\n'), 'UTF-8')",
+                "}",
+                ""
             );
         });
 
