3d9b5a794cffffbb57488a51f59d5372c5141cac
Semyon Levin <levin.semen@gmail.com>
2024-05-05 11:51:26 -0700

    [release-later] Code unification

--- src/main/java/name/remal/gradle_plugins/test_source_sets/TestSourceSetsConfigurerJacoco.java
+++ src/main/java/name/remal/gradle_plugins/test_source_sets/TestSourceSetsConfigurerJacoco.java
@@ -6,7 +6,6 @@ import static name.remal.gradle_plugins.test_source_sets.TestTaskNameUtils.getTe
 import static name.remal.gradle_plugins.toolkit.ExtensionContainerUtils.getExtension;
 import static org.codehaus.groovy.runtime.StringGroovyMethods.capitalize;
 import static org.gradle.api.tasks.SourceSet.MAIN_SOURCE_SET_NAME;
-import static org.gradle.api.tasks.SourceSet.TEST_SOURCE_SET_NAME;
 import static org.gradle.language.base.plugins.LifecycleBasePlugin.VERIFICATION_GROUP;
 
 import java.io.File;
@@ -15,6 +14,8 @@ import lombok.val;
 import org.gradle.api.Project;
 import org.gradle.api.provider.Provider;
 import org.gradle.api.tasks.SourceSetContainer;
+import org.gradle.api.tasks.TaskProvider;
+import org.gradle.api.tasks.testing.Test;
 import org.gradle.testing.jacoco.plugins.JacocoTaskExtension;
 import org.gradle.testing.jacoco.tasks.JacocoCoverageVerification;
 import org.gradle.testing.jacoco.tasks.JacocoReport;
@@ -24,60 +25,81 @@ abstract class TestSourceSetsConfigurerJacoco {
 
     public static void configureJacoco(Project project) {
         project.getPluginManager().withPlugin("jacoco", __ -> {
-            val sourceSets = getExtension(project, SourceSetContainer.class);
-            val testSourceSet = sourceSets.getByName(TEST_SOURCE_SET_NAME);
-
             val testSourceSets = getExtension(project, TestSourceSetContainer.class);
-            testSourceSets.matching(it -> it != testSourceSet).all(sourceSet -> {
-                val testTaskName = getTestTaskName(sourceSet);
-                createJacocoReportTask(project, testTaskName);
-                createJacocoCoverageVerificationTask(project, testTaskName);
+            testSourceSets.all(sourceSet -> {
+                val testTask = project.getTasks().named(getTestTaskName(sourceSet), Test.class);
+                createJacocoReportTask(project, testTask);
+                createJacocoCoverageVerificationTask(project, testTask);
             });
         });
     }
 
-    private static void createJacocoReportTask(Project project, String testTaskName) {
-        project.getTasks().register(
-            "jacoco" + capitalize(testTaskName) + "Report",
-            JacocoReport.class,
-            task -> {
-                task.mustRunAfter(testTaskName);
-                task.setGroup(VERIFICATION_GROUP);
-                task.setDescription(format(
-                    "Generates code coverage report for the %s task.",
-                    testTaskName
-                ));
-                task.executionData(createExecutionDataProvider(project, testTaskName));
-                task.sourceSets(getExtension(project, SourceSetContainer.class)
-                    .getByName(MAIN_SOURCE_SET_NAME)
-                );
-            }
-        );
+    private static void createJacocoReportTask(Project project, TaskProvider<?> testTask) {
+        val tasks = project.getTasks();
+
+        final TaskProvider<JacocoReport> jacocoReportTask;
+        val jacocoReportTaskName = "jacoco" + capitalize(testTask.getName()) + "Report";
+        if (tasks.getNames().contains(jacocoReportTaskName)) {
+            jacocoReportTask = tasks.named(jacocoReportTaskName, JacocoReport.class);
+
+        } else {
+            jacocoReportTask = tasks.register(
+                jacocoReportTaskName,
+                JacocoReport.class,
+                task -> {
+                    task.setGroup(VERIFICATION_GROUP);
+                    task.setDescription(format(
+                        "Generates code coverage report for the %s task.",
+                        testTask.getName()
+                    ));
+                    task.executionData(createExecutionDataProvider(project, testTask));
+                    task.sourceSets(getExtension(project, SourceSetContainer.class)
+                        .getByName(MAIN_SOURCE_SET_NAME)
+                    );
+                }
+            );
+        }
+
+        jacocoReportTask.configure(task -> {
+            task.mustRunAfter(testTask);
+        });
     }
 
-    private static void createJacocoCoverageVerificationTask(Project project, String testTaskName) {
-        project.getTasks().register(
-            "jacoco" + capitalize(testTaskName) + "CoverageVerification",
-            JacocoCoverageVerification.class,
-            task -> {
-                task.mustRunAfter(testTaskName);
-                task.setGroup(VERIFICATION_GROUP);
-                task.setDescription(format(
-                    "Verifies code coverage metrics based on specified rules for the %s task.",
-                    testTaskName
-                ));
-                task.executionData(createExecutionDataProvider(project, testTaskName));
-                task.sourceSets(getExtension(project, SourceSetContainer.class)
-                    .getByName(MAIN_SOURCE_SET_NAME)
-                );
-            }
-        );
+    private static void createJacocoCoverageVerificationTask(Project project, TaskProvider<?> testTask) {
+        val tasks = project.getTasks();
+
+        final TaskProvider<JacocoCoverageVerification> jacocoVerificationTask;
+        val jacocoVerificationTaskName = "jacoco" + capitalize(testTask.getName()) + "CoverageVerification";
+        if (tasks.getNames().contains(jacocoVerificationTaskName)) {
+            jacocoVerificationTask = tasks.named(jacocoVerificationTaskName, JacocoCoverageVerification.class);
+
+        } else {
+            jacocoVerificationTask = tasks.register(
+                jacocoVerificationTaskName,
+                JacocoCoverageVerification.class,
+                task -> {
+                    task.mustRunAfter(testTask);
+                    task.setGroup(VERIFICATION_GROUP);
+                    task.setDescription(format(
+                        "Verifies code coverage metrics based on specified rules for the %s task.",
+                        testTask.getName()
+                    ));
+                    task.executionData(createExecutionDataProvider(project, testTask));
+                    task.sourceSets(getExtension(project, SourceSetContainer.class)
+                        .getByName(MAIN_SOURCE_SET_NAME)
+                    );
+                }
+            );
+        }
+
+        jacocoVerificationTask.configure(task -> {
+            task.mustRunAfter(testTask);
+        });
     }
 
-    private static Provider<File> createExecutionDataProvider(Project project, String testTaskName) {
+    private static Provider<File> createExecutionDataProvider(Project project, TaskProvider<?> testTask) {
         return project.provider(() -> {
-            val testTask = project.getTasks().getByName(testTaskName);
-            val testTaskJacoco = getExtension(testTask, JacocoTaskExtension.class);
+            val testTaskJacoco = getExtension(testTask.get(), JacocoTaskExtension.class);
             return testTaskJacoco.getDestinationFile();
         });
     }
